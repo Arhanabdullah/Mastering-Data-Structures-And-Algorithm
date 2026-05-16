7ddfda11565ceecd33efb83851e45100f6b6354f
Arhan Abdullah <arhan.abdullah307@gmail.com>
2026-05-17 00:08:11 +0530

    Complete: Basic Mathematics

--- Java/A-Z-Strivers-Sheet/BasicMaths/BasicMaths.java
+++ Java/A-Z-Strivers-Sheet/BasicMaths/BasicMaths.java
@@ -88,14 +88,26 @@ public class BasicMaths {
             System.out.println(n + " is not a prime number");
         }
     }
+    public int GCD(int n1, int n2) {
+        int gcd = 0;
+        for(int i = 1 ; i<=Math.min(n1,n2); i++){
+            if(n1%i==0 && n2%i==0){
+                gcd =i;
+            }
+        }
+        return gcd;
+    }
 
     public static void main(String[] args) {
-        int number = 3;
+        int number1 = 3;
+        int number2 = 6;
+        BasicMaths bm = new BasicMaths();
         // numberOfDigits(number);
         // reverseOfNumber(number);
         // palindromeNumber(number);
         // armstrongNumber(number);
         // printAllDivisions(number);
-        primeNumbers(number);
+        // primeNumbers(number);
+        bm.GCD(number1, number2);
     }
 }
