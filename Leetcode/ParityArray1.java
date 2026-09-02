a85993f03e20e8a4d22922efba0e6a51482ed590
Arhan Abdullah <arhan.abdullah307@gmail.com>
2026-09-02 22:40:35 +0530

    add: Leetcode 3875

---
+++ Leetcode/ParityArray1.java
@@ -0,0 +1,11 @@
+public class ParityArray1 {
+// leetcode 3875. Construct Uniform Parity Array 1
+// Time Complexity: O(1) space Complexity: O(1) space
+
+    class Solution {
+        public boolean uniformArray(int[] nums1) {
+            return true;
+        }
+    }
+
+}
